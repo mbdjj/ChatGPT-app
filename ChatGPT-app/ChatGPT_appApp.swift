9ca9c5ed01ea0599b46c0604f2c7be693a3e0bfb
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-06 14:35:15 +0100

    Initial Commit

---
+++ ChatGPT-app/ChatGPT_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_appApp.swift
+//  ChatGPT-app
+//
+//  Created by Marcin Bartminski on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
